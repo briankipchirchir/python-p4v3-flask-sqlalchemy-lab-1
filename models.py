09767c59854d3d66071e8dd286ba8709bac1c3c9
briankipchirchir <kbrianngetich@gmail.com>
2024-10-01 14:57:25 +0300

    kk

---
+++ models.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
+
+
+metadata = MetaData()
+
+db = SQLAlchemy(metadata=metadata)
+
+# Add models here
+class Earthquake(db.Model,SerializerMixin):
+    __tablename__ = 'earthquakes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    depth = db.Column(db.Float, nullable=True)  # Add depth attribute
+
+
+
+    def __repr__(self):
+        return f"<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>"
+    
+   
